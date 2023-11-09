d6e9e0483cbe38d38d5a6129c83562b95da5824c
GabrielCLC <gabrielclcardoso@gmail.com>
2023-11-09 16:49:44 -0300

    feat: created fetchBraveApi

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { fetchBraveApi } from "./Search";
+import "./App.css";
 
-function App() {
+export function TestButton() {
+  async function get() {
+    fetchBraveApi(
+      "https://api.search.brave.com/res/v1/web/search?q=nym&count=1&result_filter=web",
+    );
+  }
+  return <button onClick={get}>Search</button>;
+}
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TestButton />
     </div>
   );
 }
-
-export default App;
