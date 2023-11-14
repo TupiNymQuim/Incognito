6759c6e124d1ee9c0b0f6af6b7ff896011a32fa1
Vitor Sergio <vitorsergio111@gmail.com>
2023-11-14 02:58:56 -0300

    refactor: Web & News button with better visual and functionality

--- front/components/Header/index.tsx
+++ front/components/Header/index.tsx
@@ -2,10 +2,23 @@ import styles from "./styles.module.css";
 import Image from "next/image";
 import logoImg from "../../assets/logo.png";
 import searchImg from "../../assets/search.png";
+import { ReadOutlined, GlobalOutlined } from "@ant-design/icons";
 import { useState } from "react";
 
-export function Header() {
-  const [selected, setSelected] = useState("");
+type Props = {
+  currentInput: string;
+  resultType: string;
+  handleSearch: (newInput: string) => void;
+  handleChangeType: (newType: string) => void;
+};
+
+export function Header({
+  currentInput,
+  resultType,
+  handleSearch,
+  handleChangeType,
+}: Props) {
+  const [input, setInput] = useState(currentInput);
 
   return (
     <div className={styles.header}>
@@ -23,22 +36,27 @@ export function Header() {
             className={styles.inputIcon}
             src={searchImg}
           ></Image>
-          <input></input>
+          <input
+            onKeyDown={(e) => e.key === "Enter" && handleSearch(input)}
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          ></input>
         </div>
       </div>
       <div className={styles.buttons}>
-        <button onClick={() => setSelected("Web")} className={styles.button}>
-          Web
+        <button
+          className={resultType === "Web" ? styles.activeButton : styles.button}
+          onClick={() => handleChangeType("Web")}
+        >
+          <GlobalOutlined /> Web
         </button>
         <button
-          type="button"
-          onClick={() => setSelected("Images")}
-          className={styles.button}
+          className={
+            resultType === "News" ? styles.activeButton : styles.button
+          }
+          onClick={() => handleChangeType("News")}
         >
-          Images
-        </button>
-        <button onClick={() => setSelected("News")} className={styles.button}>
-          News
+          <ReadOutlined /> News
         </button>
       </div>
     </div>
